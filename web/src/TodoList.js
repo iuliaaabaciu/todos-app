893c7d527b42bd0e09308bbaa0b4633a627f0479
iuliab <iuliaiuliabaciu@yahoo.com>
2020-04-28 20:06:55 +0300

    course changes

--- web/src/TodoList.js
+++ web/src/TodoList.js
@@ -15,12 +15,44 @@ class TodoList extends Component {
   checkTodo = async (id, isCompleted) => {
     await updateTodo(id, isCompleted);
     const todos = await listTodos(); 
-    this.setState({ todos: todos }) 
+    this.setState({ todos: todos });
   }; 
   
+  deleteTodo = async(id) => {
+    await deleteTodo(id);
+    const todos = await listTodos(); 
+    this.setState({ todos: todos }); 
+  }
+
+  setFilterToAll = () => {
+    this.setState({ filter: "ALL"});
+  }
+
+  setFilterToCompleted = () => {
+    this.setState({ filter: "COMPLETED"});
+  }  
+
+  setFilterToIncompleted = () => {
+    this.setState({ filter: "INCOMPLETED"});
+  } 
+
+  filterTodos = (todos, filter) => {
+    if (filter === 'ALL') {
+      return TodoList;
+    } 
+
+    if (filter === 'COMPLETED') {
+      return todos.filter((element) => element.isCompleted === 1); // daca element.isCompelted === true, atunci returnam true
+    }
+
+    if (filter === 'INCOMPLETED') {
+      return todos.filter((element) => element.isCompleted === 0); // daca element.isCompelted === true, atunci returnam true
+    }    
+  }
+
   async componentDidMount() {
     const todos = await listTodos(); //await pt ca vreau todos din promisuunea respectiva
-    this.setState({ todos: todos }) 
+    this.setState({ todos: todos }); 
   }
 
   onInputChange = (event) => {
@@ -32,31 +64,41 @@ class TodoList extends Component {
     if(event.nativeEvent.keyCode === 13) {
       await createTodo(this.state.inputValue, false);
       const todos = await listTodos(); 
-      this.setState({ todos: todos }) 
-    }
+      this.setState({ todos: todos, inputValue: "" });     }
   }
 
     render() {
+      const filteredTodos = this.filterTodos(this.state.todos, this.state.filter);
+
       return (
-        <div>
-          <input className='input' 
+        <div className="container">
+          <input className="input" 
                 value={this.state.inputValue}
                 onChange={this.onInputChange}
                 onKeyPress={this.onInputKeyPress}
           />
 
-          {this.state.todos.map((element) => {
-            return <label key={element.id} className="todo">
-              <input 
-                type="checkbox" 
-                checked={ element.isCompleted } 
-                onChange={(event) => {
-                  this.checkTodo(element.id, event.target.checked)
-                }} 
-              />
-              { element.title }
-            </label>
-          })}
+          <div className="todosList">
+            {this.state.todos.map((element) => {
+              return <label key={element.id} className="todo">
+                <input 
+                  type="checkbox" 
+                  checked={ element.isCompleted } 
+                  onChange={(event) => {
+                    this.checkTodo(element.id, event.target.checked)
+                  }} 
+                />
+                { element.title } 
+                <button onClick={ () => this.deleteTodo(element.id)}>x</button>
+              </label>
+            })}
+          </div>
+
+          <div>
+            <button onClick={this.setFilterToAll}>All</button>
+            <button onClick={this.setFilterToCompleted}>Completed</button>
+            <button onClick={this.setFilterToIncompleted}>Incompleted</button>
+          </div>
         </div>
       );
     }
